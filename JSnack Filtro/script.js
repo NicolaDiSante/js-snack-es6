1441670afe8dfbeb6004984bcdf97f1027c00d5a
NicolaDiSante <nickymacha90@gmail.com>
2021-04-29 09:20:10 +0200

    Errore da risolvere

---
+++ JSnack Filtro/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  // Dichiarare array e due variabili numero
+  let arrayLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  let limiteMin = 2;
+  let limiteMax = 7;
+
+  
+
+  // Dichiarare la funzione con parametri: array, n1, n2
+  const getPieceOfArray = (arr, min, max) => {
+    // Dentro la funzione creare un array vuoto
+    
+    let newArr = [];
+    // Facciamo un ciclo for per prelevare i valori di arrItems compresi tra minimo e massimo
+    for(let i of arr){
+        // Pusharli nell'array vuoto 
+      if(i >= min && i<=max) newArr.push(arr[i])
+    }
+    // Restituire il risultato dell'array
+    return newArr;
+
+  }
+  console.log(getPieceOfArray(arrayLetter, limiteMin, limiteMax))
+    
+  })
+
+  
+  
+   

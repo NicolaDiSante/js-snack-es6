1caae514eef97495bd578ab7d8ffb4358a5ffe73
NicolaDiSante <nickymacha90@gmail.com>
2021-05-03 15:22:24 +0200

    Completato

--- Jsnack array, oggetti, map, interpolazione delle stringhe/script.js
+++ Jsnack array, oggetti, map, interpolazione delle stringhe/script.js
@@ -2,5 +2,15 @@ const persone = [
 	{ nome: 'Mario', cognome: 'Rossi', eta: 60 },
 	{ nome: 'Luigi', cognome: 'Verdi', eta: 12 },
 	{ nome: 'Silvia', cognome: 'Neri', eta: 25 },
-]
+];
 
+
+const abilitatoAllaGuida = persone.map((persona) => {
+	if(persona.eta > 18){
+		return persona.nome + " Può guidare!"
+	}else{
+		return persona.nome + " Non può guidare!"
+	}
+});
+
+console.log(abilitatoAllaGuida)
